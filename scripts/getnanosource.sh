95ea10c81f82b197370189fd5d8d159fd670068f
V Bota <VBota1@users.noreply.github.com>
2017-07-01 16:40:57 +0300

    added GPL3 license elements

--- scripts/getnanosource.sh
+++ scripts/getnanosource.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+: <<'LICENSE'
+    Copyright (C) {2017}  {Bota Viorel}
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+LICENSE
+
 handle_error ()
 {
         echo "ERROR: $@"
@@ -7,6 +24,11 @@ handle_error ()
 }
 
 echo "TITLE: GET NANO SOURCE CODE 2 STEPS"
+
+echo "Copyright (C) {2017}  {Bota Viorel}
+ This program comes with ABSOLUTELY NO WARRANTY.
+ This is free software, and you are welcome to redistribute it under the GPL conditions. 
+"
 
 if [ -z "${1}" ]; then
         handle_error "missing nano archive name"
