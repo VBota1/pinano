1a5dd7c3624c516200dd3314bff1bc4f007f40d9
V Bota <VBota1@users.noreply.github.com>
2017-07-21 22:43:43 +0300

    fixed problem with detecting yaml file

--- scripts/snapnano.sh
+++ scripts/snapnano.sh
@@ -1,22 +1,5 @@
 #!/bin/sh
 
-: <<'LICENSE'
-    Copyright (C) {2017}  {Bota Viorel}
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program. If not, see <http://www.gnu.org/licenses/>.
-LICENSE
-
 handle_error ()
 {
         echo "ERROR: $@"
@@ -24,11 +7,6 @@ handle_error ()
 }
 
 echo "TITLE: PACK NANO IN PINANO SNAP AND UPLOAD TO UBUNTU STORE. 5 STEPS"
-
-echo "Copyright (C) {2017}  {Bota Viorel}
- This program comes with ABSOLUTELY NO WARRANTY.
- This is free software, and you are welcome to redistribute it under the GPL conditions. 
-"
 
 if [ -z "${1}" ]; then
         handle_error "missing nano archive name"
@@ -44,11 +22,11 @@ echo "Step 1 of 5: create snapcraft.yaml"
 cd ../scripts
 python createsnapcraft_yaml.py $1
 
+cd ../snap
+
 if [ ! -f snapcraft.yaml ]; then
         handle_error "snapcraft.yaml was not created"
 fi
-
-cd ../snap
 
 echo "Step 2 of 5: clean pull (needed to update source)"
 snapcraft clean pinano -s pull
